e587ece7f8307817dce0a457fc9c525bef3f54e9
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-18 17:03:31 -0500

    commit

--- bton_market_backend/settings.py
+++ bton_market_backend/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-awv0p75oyj24hs+yyn+w$h4rvrz$qkr@6_g9zw4*l89zj54^z!'
+
+# SECURITY WARNING: keep the secret key used in production secret!  
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
