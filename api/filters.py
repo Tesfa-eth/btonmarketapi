c6b4679501d356a34d01ad04bc245c68c01aaf93
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-12-03 23:29:46 -0500

    search by item name

---
+++ api/filters.py
@@ -0,0 +1,7 @@
+# from .models import OnSaleItems
+# from django_filters import FilterSet
+
+# class OnSaleItemsFilter(FilterSet):
+#     class Meta:
+#         model = OnSaleItems
+#         fields = ['name', 'seller']
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('onsale-items/', views.onSellItemsList, name="onsale-items"),
+    path("searchItem/<str:itemname>", views.filterOnSellItems, name="searchItem"),
     path('onsale-create/', views.onSellItemsCreate, name="onsale-items-detail")
 ]
