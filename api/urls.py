746c87a9380118d6e2ca1af5494a2e1d3f7d211d
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-12-06 19:32:48 -0500

    debug added to url

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     path("onsale-item-detail/<str:pk>", views.itemDetail, name="itemDetail"),
     path('onsale-create/', views.onSellItemsCreate, name="onsale-items-detail")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
